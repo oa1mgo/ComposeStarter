35e87c9613551e07d91ae93aba99f35dfba486db
miaojiaxin <37007585+miaojiaxin1995@users.noreply.github.com>
2023-10-22 22:06:13 +0800

    fix(splash): Fix the bug of the splash screen jumping to the MainActivity twice

--- app/src/main/java/net/taikula/compose/starter/splash/SplashActivity.kt
+++ app/src/main/java/net/taikula/compose/starter/splash/SplashActivity.kt
@@ -4,12 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock.sleep
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -18,18 +16,16 @@ import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.green
-import androidx.core.graphics.red
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -38,7 +34,6 @@ import net.taikula.compose.starter.R
 import net.taikula.compose.starter.base.BaseComponentActivity
 import net.taikula.compose.starter.ui.theme.ComposeStaterTheme
 import kotlin.math.ceil
-import kotlin.math.roundToInt
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseComponentActivity() {
@@ -52,12 +47,17 @@ class SplashActivity : BaseComponentActivity() {
                         .fillMaxSize()
                         .background(colorScheme.background)
                 ) {
+                    var hasFinish by remember { mutableStateOf(false) }
                     CountdownText(
                         modifier = Modifier
                             .align(Alignment.TopEnd)
                             .padding(16.dp),
                         initialTime = 5_000L,
                     ) {
+                        if (hasFinish) {
+                            return@CountdownText
+                        }
+                        hasFinish = true
                         startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                         finish()
                     }
